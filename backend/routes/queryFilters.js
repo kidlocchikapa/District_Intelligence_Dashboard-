afa3111ee8108414fa43c2db5a803173c1da1585
Pre-pukulu <bed-com-50-21@unima.ac.mw>
2026-04-19 23:00:34 +0200

    fix: education insights query shape

--- backend/routes/queryFilters.js
+++ backend/routes/queryFilters.js
@@ -1,10 +1,10 @@
 /**
- * 
- * @param {*} conditions 
- * @param {*} params 
- * @param {*} columnExpression 
- * @param {*} district 
- * @returns 
+ *
+ * @param {*} conditions
+ * @param {*} params
+ * @param {*} columnExpression
+ * @param {*} district
+ * @returns
  */
 function appendDistrictNameCondition(
   conditions,
@@ -20,14 +20,13 @@ function appendDistrictNameCondition(
   conditions.push(`LOWER(${columnExpression}) = LOWER($${params.length})`);
 }
 
-
 /**
- * 
- * @param {*} conditions 
- * @param {*} params 
- * @param {*} geometryExpression 
- * @param {*} district 
- * @returns 
+ *
+ * @param {*} conditions
+ * @param {*} params
+ * @param {*} geometryExpression
+ * @param {*} district
+ * @returns
  */
 function appendDistrictGeometryCondition(
   conditions,
@@ -44,9 +43,8 @@ function appendDistrictGeometryCondition(
   conditions.push(`
       EXISTS (
         SELECT 1
-        FROM administrative_units district_filter
+        FROM districts district_filter
         WHERE district_filter.geom IS NOT NULL
-          AND LOWER(district_filter.type) = LOWER('District')
           AND LOWER(district_filter.name) = LOWER(${districtParam})
           AND ST_Intersects(${geometryExpression}, district_filter.geom)
       )
