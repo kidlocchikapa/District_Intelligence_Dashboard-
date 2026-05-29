8146380a03ce02e3e0550cae390ab9645e327e63
Kidloc Chikapa <kidloc24chikapa@gmail.com>
2026-05-29 12:37:29 +0200

    updated label on edu

--- frontend/src/Pages/EducationPage.jsx
+++ frontend/src/Pages/EducationPage.jsx
@@ -1866,7 +1866,7 @@ function EducationPage() {
                   },
                   {
                     key: "school_population_unserved_total",
-                    label: `Outside ${EDUCATION_ACCESS_DISTANCE_LABEL}`,
+                    label: `Unenrolled ${EDUCATION_ACCESS_DISTANCE_LABEL}`,
                     digits: 0,
                   },
                 ]}
